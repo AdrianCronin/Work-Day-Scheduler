74c99fbf7282ee3ae2fbf45990631a7fd6d23bdc
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-06-05 09:54:13 -0700

    play with input forms

--- assets/script.js
+++ assets/script.js
@@ -25,27 +25,28 @@ var timeTableEl = $('#timeTable');
 //     `);
 // };
 
-for (var i=0; i<=8; i++) {
-    var hour = i + 9;
-    var ampm = 'am';
-    if (hour === 12) {
-        ampm = 'pm';
-    } else if (hour > 12) {
-        hour -= 12;
-        ampm = 'pm';
-    };
-    // try appending data-attributes 
-    timeTableEl.append(`
-        <div class="row">
-          <div class="col-1 helpBorder">
-            ${hour}${ampm}
-          </div>
-          <div class="col-10 helpBorder">
-            Meeting ${i + 1}
-          </div>
-          <div class="col-1 helpBorder">
-            save button
-          </div>
-        </div>
-    `);
-};
+// // column for loop
+// for (var i=0; i<=8; i++) {
+//     var hour = i + 9;
+//     var ampm = 'am';
+//     if (hour === 12) {
+//         ampm = 'pm';
+//     } else if (hour > 12) {
+//         hour -= 12;
+//         ampm = 'pm';
+//     };
+//     // try appending data-attributes 
+//     timeTableEl.append(`
+//         <div class="row">
+//           <div class="col-1 helpBorder">
+//             ${hour}${ampm}
+//           </div>
+//           <div class="col-10 helpBorder">
+//             Meeting ${i + 1}
+//           </div>
+//           <div class="col-1 helpBorder">
+//             save button
+//           </div>
+//         </div>
+//     `);
+// };
