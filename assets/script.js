5802d4c58b479e40edc3ff2f00010cfd1b4ad573
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-06-04 21:29:09 -0700

    made a for loop with template literals to create the html table

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,24 @@ var currentDayEl = $('#currentDay');
 
 currentDayEl.text(moment().format('dddd, MMMM Do'));
 
+var timeTableEl = $('#timeTable');
+
+for (var i=0; i<=8; i++) {
+    var hour = i + 9;
+    var ampm = 'am';
+    if (hour === 12) {
+        ampm = 'pm';
+    }
+    if (hour > 12) {
+        hour = hour - 12;
+        ampm = 'pm';
+    };
+    timeTableEl.append(`
+        <tr>
+        <th scope="row">${hour}${ampm}</th>
+        <td colspan="3">Meeting 1</td>
+        <td>@</td>
+        </tr>
+    `);
+};
+
