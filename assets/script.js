d6132cb8ac0ecc3426e9fc682be35835c4ea9ee1
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-06-05 09:12:31 -0700

    made a for-loop with template literal to create rows and columns without a table

--- assets/script.js
+++ assets/script.js
@@ -4,21 +4,48 @@ currentDayEl.text(moment().format('dddd, MMMM Do'));
 
 var timeTableEl = $('#timeTable');
 
+
+
+// // for loop to generate table rows
+// for (var i=0; i<=8; i++) {
+//     var hour = i + 9;
+//     var ampm = 'am';
+//     if (hour === 12) {
+//         ampm = 'pm';
+//     } else if (hour > 12) {
+//         hour = hour - 12;
+//         ampm = 'pm';
+//     };
+//     timeTableEl.append(`
+//         <tr>
+//         <th scope="row">${hour}${ampm}</th>
+//         <td colspan="3">Meeting ${i+1}</td>
+//         <td>@</td>
+//         </tr>
+//     `);
+// };
+
 for (var i=0; i<=8; i++) {
     var hour = i + 9;
     var ampm = 'am';
     if (hour === 12) {
         ampm = 'pm';
     } else if (hour > 12) {
-        hour = hour - 12;
+        hour -= 12;
         ampm = 'pm';
     };
+    // try appending data-attributes 
     timeTableEl.append(`
-        <tr>
-        <th scope="row">${hour}${ampm}</th>
-        <td colspan="3">Meeting ${i+1}</td>
-        <td>@</td>
-        </tr>
+        <div class="row">
+          <div class="col-1 helpBorder">
+            ${hour}${ampm}
+          </div>
+          <div class="col-10 helpBorder">
+            Meeting ${i + 1}
+          </div>
+          <div class="col-1 helpBorder">
+            save button
+          </div>
+        </div>
     `);
 };
-
