02adcf8c8f97dc51cef0e72513252fee6a50a5e5
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-06-05 18:27:35 -0700

    added comments and moved the for loop that creates the columns into a function

--- assets/script.js
+++ assets/script.js
@@ -5,27 +5,31 @@ currentDayEl.text(moment().format('dddd, MMMM Do'));
 var timeTableEl = $('#timeTable');
 
 // // column for loop
-// for (var i=0; i<=8; i++) {
-//     var hour = i + 9;
-//     var ampm = 'am';
-//     if (hour === 12) {
-//         ampm = 'pm';
-//     } else if (hour > 12) {
-//         hour -= 12;
-//         ampm = 'pm';
-//     };
-//     // try appending data-attributes 
-//     timeTableEl.append(`
-//         <div class="row">
-//           <div class="col-1 helpBorder">
-//             ${hour}${ampm}
-//           </div>
-//           <div class="col-10 helpBorder">
-//             Meeting ${i + 1}
-//           </div>
-//           <div class="col-1 helpBorder">
-//             save button
-//           </div>
-//         </div>
-//     `);
-// };
+function renderTable() {
+    for (var i=0; i<=8; i++) {
+        var hour = i + 9;
+        var ampm = 'am';
+        if (hour === 12) {
+            ampm = 'pm';
+        } else if (hour > 12) {
+            hour -= 12;
+            ampm = 'pm';
+        };
+        // try appending data-attributes 
+        timeTableEl.append(`
+            <div class="row">
+            <div class="col-1 helpBorder">
+                ${hour}${ampm}
+            </div>
+            <div class="col-10 helpBorder">
+                <textarea class="meetingPlan" placeholder="Add Meeting"></textarea>
+            </div>
+            <div class="col-1 helpBorder">
+                save button
+            </div>
+            </div>
+        `);
+    };
+};
+
+renderTable();
