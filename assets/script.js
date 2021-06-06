6266d1fb9bd1cd3e02e11efcdac4b16e7297bd40
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-06-06 13:06:18 -0700

    save buttons now save their corresponding textarea text to local storage

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,9 @@ var timeTableEl = $('#timeTable');
 var currentHour = moment().format('HH'); // the current hour
 
 currentDayEl.text(moment().format('dddd, MMMM Do')); // display day of week and month
+renderTable(); // any references to HTML elements created by this function but be done after this call
+
+var saveColumnEl = $('.saveColumn'); // save buttons' parent element reference, only works after renderTable() executes
 
 // this function builds the html elements that make up the planner
 function renderTable() {
@@ -22,27 +25,43 @@ function renderTable() {
         // it also generates dataset attributes and id's depending on the hour of the day they correspond to
         timeTableEl.append(`
             <div class="row">
+
                 <div class="col-1 hourColumn">
                     ${hour}${ampm} 
                 </div>
+
                 <div class="col-10" data-hour="${dataHour}" id="idHour${dataHour}">
-                    <textarea class="meetingPlan" placeholder=""></textarea>
+                    <textarea class="meetingPlan" id="textArea${dataHour}"></textarea>
                 </div>
-                <div class="col-1 saveColumn">
 
-                    <i class="bi bi-save-fill"></i>
-
+                <div class="col-1 saveColumn" data-savebutton="${dataHour}">
+                    <i class="bi bi-save-fill" data-savebutton="${dataHour}"></i>
                 </div>
+
             </div>
         `);
 
-        var idString = "#idHour" + dataHour; // string with the element Id 
-        var idHourEl = $(idString); // reference to element to pass as an argument
+        var idHourString = "#idHour" + dataHour; // string with the element Id 
+        var idHourEl = $(idHourString); // reference to element to pass as an argument
         var hourValue = idHourEl.data("hour"); // storing element's dataset attribute to pass as an argument
 
-        colorRows(hourValue, idHourEl); 
-    };
+        // var textAreaString ="#textArea" + dataHour;
+        // var textAreaEl = $(textAreaString);
+   
+        colorRows(hourValue, idHourEl);
+    }; // end of for loop
+
 };
+
+// press save icon to save corresponding text area
+saveColumnEl.click(function saveInputs(event) {
+    var target = event.target.getAttribute("data-savebutton");
+    var x = 'textArea' + target
+    var y = document.getElementById(x).value;
+
+    localStorage.setItem(x, y); // textarea Id used as key, text is the value
+});
+
 
 // this function colors the background of the text areas by adding css classes
 function colorRows(calendarHour, element) {
@@ -55,21 +74,10 @@ function colorRows(calendarHour, element) {
     };
 };
 
-renderTable();
-
-// `saveInputs()`
-//  trigger on a savebutton press event maybe
-// saves current text in the corresponding box to memory
-
-// `saveHover()`
-//  trigger when save buttons are hovered
-//  change the floppy disk 
-
-var saveColumnEl = $('.saveColumn'); // save buttons parent element reference
-
 // changes icon to black when hovering, changes back to white when not hovering
 saveColumnEl.hover(function(event) {
     event.target.style.color ='#000000'
 }, function(event) {
     event.target.style.color ='#ffffff'
 });
+
