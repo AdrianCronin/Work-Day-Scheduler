727510dc43cf388c31e72f299ded616101f62e1b
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-06-05 21:04:06 -0700

    added comments to javascript

--- assets/script.js
+++ assets/script.js
@@ -4,23 +4,26 @@ var currentHour = moment().format('HH'); // the current hour
 
 currentDayEl.text(moment().format('dddd, MMMM Do')); // display day of week and month
 
-// column for loop
+// this function builds the html elements that make up the planner
 function renderTable() {
     for (var i=0; i<=8; i++) {
-        var hour = i + 9;
-        var dataHour = i + 9;
+        var hour = i + 9; // work day starts at 9am 
+        var dataHour = i + 9; // this variable keeps track of the hour in 24h format
         var ampm = 'AM';
+
         if (hour === 12) {
-            ampm = 'PM';
+            ampm = 'PM'; // change AM to PM at noon
         } else if (hour > 12) {
-            hour -= 12;
-            ampm = 'PM';
+            hour -= 12; // reset hour to 1 after noon
+            ampm = 'PM'; // change AM to PM in the afternoon
         };
-        // try appending data-attributes 
+
+        // this template literal creates the rows and columns filling in the hours and AM/PM from the variables above
+        // it also generates dataset attributes and id's depending on the hour of the day they correspond to
         timeTableEl.append(`
             <div class="row">
                 <div class="col-1 hourColumn">
-                    ${hour}${ampm}
+                    ${hour}${ampm} 
                 </div>
                 <div class="col-10" data-hour="${dataHour}" id="idHour${dataHour}">
                     <textarea class="meetingPlan" placeholder="Add Meeting"></textarea>
@@ -30,19 +33,17 @@ function renderTable() {
                 </div>
             </div>
         `);
-        var idString = "#idHour" + dataHour;
-        var idHourEl = $(idString);
-        var hourValue = idHourEl.data("hour");
-        // console.log(hourValue);
-        colorRows(hourValue, idHourEl);
+
+        var idString = "#idHour" + dataHour; // string with the element Id 
+        var idHourEl = $(idString); // reference to element to pass as an argument
+        var hourValue = idHourEl.data("hour"); // storing element's dataset attribute to pass as an argument
+
+        colorRows(hourValue, idHourEl); 
     };
 };
 
-renderTable();
-
+// this function colors the background of the text areas by adding css classes
 function colorRows(calendarHour, element) {
-    console.log(calendarHour);
-    console.log(element);
     if (currentHour == calendarHour) {
         element.addClass('currentHour');
     } else if (currentHour < calendarHour) {
@@ -52,4 +53,5 @@ function colorRows(calendarHour, element) {
     };
 };
 
+renderTable();
 
