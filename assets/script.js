243fe665f99158b22d7e51cca1d018b492921b89
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-06-05 18:09:21 -0700

    added a css class for the text areas to fill their containers, also moved some code I may or may not revisit into an archive text file, will remove later

--- assets/script.js
+++ assets/script.js
@@ -4,27 +4,6 @@ currentDayEl.text(moment().format('dddd, MMMM Do'));
 
 var timeTableEl = $('#timeTable');
 
-
-
-// // for loop to generate table rows
-// for (var i=0; i<=8; i++) {
-//     var hour = i + 9;
-//     var ampm = 'am';
-//     if (hour === 12) {
-//         ampm = 'pm';
-//     } else if (hour > 12) {
-//         hour = hour - 12;
-//         ampm = 'pm';
-//     };
-//     timeTableEl.append(`
-//         <tr>
-//         <th scope="row">${hour}${ampm}</th>
-//         <td colspan="3">Meeting ${i+1}</td>
-//         <td>@</td>
-//         </tr>
-//     `);
-// };
-
 // // column for loop
 // for (var i=0; i<=8; i++) {
 //     var hour = i + 9;
