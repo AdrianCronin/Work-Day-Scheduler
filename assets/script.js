7bd49bf56a8e71f779f9763a04ae5bd9a3b6ed57
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-06-05 18:59:15 -0700

    added hourColumn css class and added it to the first column

--- assets/script.js
+++ assets/script.js
@@ -7,25 +7,25 @@ currentDayEl.text(moment().format('dddd, MMMM Do')); // display day of week and 
 function renderTable() {
     for (var i=0; i<=8; i++) {
         var hour = i + 9;
-        var ampm = 'am';
+        var ampm = 'AM';
         if (hour === 12) {
-            ampm = 'pm';
+            ampm = 'PM';
         } else if (hour > 12) {
             hour -= 12;
-            ampm = 'pm';
+            ampm = 'PM';
         };
         // try appending data-attributes 
         timeTableEl.append(`
             <div class="row">
-            <div class="col-1 helpBorder">
-                ${hour}${ampm}
-            </div>
-            <div class="col-10 helpBorder">
-                <textarea class="meetingPlan" placeholder="Add Meeting"></textarea>
-            </div>
-            <div class="col-1 helpBorder">
-                💾
-            </div>
+                <div class="col-1 hourColumn">
+                    ${hour}${ampm}
+                </div>
+                <div class="col-10 ">
+                    <textarea class="meetingPlan" placeholder="Add Meeting"></textarea>
+                </div>
+                <div class="col-1 ">
+                    💾
+                </div>
             </div>
         `);
     };
